df60bb7c5be5d26c0d6cfdb4f53d28f2747e8aac
Mamad Jadali <jadalimamad@gmail.com>
2023-12-25 22:43:06 +0330

    Core Customasation
    Footer
    Header

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,22 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: "en",
+    locales: {
+          en: {
+            label: 'English',
+            lang: 'en', // lang is required for root locales
+          },
+          fa: {
+              label: 'Persian',
+              lang: 'fa',
+              dir: 'rtl',
+          },
+    },
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });
