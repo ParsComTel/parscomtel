12a132471615cfbc38ec48accb089ac421a9e100
Mamad Jadali <jadalimamad@gmail.com>
2023-12-30 19:56:21 +0330

    animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,7 +16,7 @@ module.exports = {
       current: colors.current,
       transparent: colors.transparent,
       primary: "#7692ff",
-      secondary: "#ff7e33",
+      secondary: "#ee66aa",
       info: "#0C63E7",
       black: colors.black,
       white: colors.white,
