c78d05d60ef8efc4c49fe7887a5e3169dbcf1e78
Mamad Jadali <jadalimamad@gmail.com>
2023-12-30 20:01:43 +0330

    sit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  i18n: {
-    defaultLocale: "en",
-    locales: {
-          en: {
-            label: 'English',
-            lang: 'en', // lang is required for root locales
-          },
-          fa: {
-              label: 'Persian',
-              lang: 'fa',
-              dir: 'rtl',
-          },
-    },
-    routing: {
-      prefixDefaultLocale: false
-    }
-  }
+    site: "https://parscomtel.github.io",
+    base: "/parscomtel"
 });
